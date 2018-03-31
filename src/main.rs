ee283cffb61a403a737b29a5a384e15d2b4a20b5
Corentin Jechoux <cjechoux@algofi.fr>
2018-03-31 21:03:53 +0200

    simple main exemple

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ extern crate permutation_way;
 
 use permutation_way::permute;
 
+use std::time::Instant;
+
 fn main() {
 
 
 
-    let input = vec![1, 2, 3];
+    let input = vec![1, 2, 3, 4];
 
-    permute( input );
+
+    let start = Instant::now();
+
+    let permutations = permute( input );
+
+    let end = Instant::now();
+
+    println!("Execution time = {:?}", end.duration_since(start));
+    println!("Permutation found {}", permutations.len() );
+
+
+
+
 
 }
