130b9165f74126824a0b2f6fbad1a88d7f33c1dd
Corentin Jechoux <cjechoux@algofi.fr>
2018-03-31 21:04:18 +0200

    simple integration test ipnut 1, 2, 3

---
+++ tests/integration_test.rs
@@ -0,0 +1,27 @@
+extern crate permutation_way;
+
+use permutation_way::permute;
+
+#[test]
+fn should_find_6_uniq_permutation_given_a_vec_of_3_elements () {
+
+    // input
+    let input = vec![1, 2, 3];
+
+    // call
+    let permutations = permute( input );
+
+    // assertions
+    assert_eq!( 6, permutations.len() );
+    assert_eq!( vec![
+        vec![1, 2, 3],
+        vec![1, 3, 2],
+        vec![3, 1, 2],
+        vec![3, 2, 1],
+        vec![2, 3, 1],
+        vec![2, 1, 3]
+    ], permutations );
+
+
+
+}
