c22b8a642ae61364de10f6a12a3686388126ae75
Corentin Jechoux <cjechoux@algofi.fr>
2018-04-07 00:58:09 +0200

    implementation iterator

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,77 @@ use mobility::create_directions;
 
 use utils::print_permutation;
 use utils::find_largest_mobile_element;
-use utils::Largest;
+
+
+pub struct PermutationEngine {
+    input: Vec<i32>,
+    directions: Vec<Mobility>,
+    counter: u32,
+}
+
+impl PermutationEngine {
+    pub fn new( input : Vec<i32> ) -> PermutationEngine {
+
+        let directions = create_directions( &input );
+
+        let mut engine = PermutationEngine{ input,
+            directions,
+            counter: 0 };
+
+        engine.input.sort();
+
+        engine
+    }
+}
+
+impl Iterator for PermutationEngine {
+    type Item = Vec<i32>;
+
+    fn next( &mut self ) -> Option<Self::Item> {
+
+
+        if self.counter == 0 {
+            println!( "         COUNTER 0" );
+            self.counter = self.counter + 1;
+            return Some( self.input.clone() );
+        }
+
+        let largest = find_largest_mobile_element(&self.input, &self.directions);
+        let direction = largest.direction;
+        let mobile_position = largest.position;
+
+        if direction == NotMobile {
+            self.counter = self.counter + 1;
+            return None;
+        }
+
+        let swap_result = direction.swap( &mut self.input, &mut self.directions, mobile_position );
+
+        /*if let Err( SwapError(position)) = swap_result {
+            return Err(format!("swap permutation error at position {}", position));
+        }*/
+        let result = Some( self.input.clone() );
+
+
+        let reset_result = direction.reset( &self.input, &mut self.directions, mobile_position );
+
+        /*if let Err(SwapError(position)) = reset_result {
+            return Err(format!("swap permutation error at position {}", position));
+        }
+        if let Err(ResetError(position)) = reset_result {
+            return Err(format!( "reset permutation error at position {}", position));
+        }
+        */
+        //print_permutation( &input, &directions );
+
+
+        self.counter = self.counter + 1;
+        result
+
+
+    }
+}
+
 
 
 /// find all permutations of the given `input` vector.
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,7 @@
 extern crate permutation_way;
 
 use permutation_way::permute;
+use permutation_way::PermutationEngine;
 
 #[test]
 fn should_find_6_uniq_permutation_given_a_vec_of_3_elements () {
@@ -34,4 +35,31 @@ fn should_return_an_empty_vec_given_an_empty_array() {
     // assertions
     assert_eq!( Err( String::from("Cannot find permutation on empty vec") ), permutations );
 
+}
+
+#[test]
+fn should_find_6_permutation_given_a_vec_of_3_elements_with_iterator() {
+    // input
+    let input = vec![1, 2, 3];
+    // call
+    let mut engine = PermutationEngine::new( input );
+    // assertions
+    assert_eq!( Some( vec![1, 2, 3] ), engine.next() );
+    assert_eq!( Some( vec![1, 3, 2] ), engine.next() );
+    assert_eq!( Some( vec![3, 1, 2] ), engine.next() );
+    assert_eq!( Some( vec![3, 2, 1] ), engine.next() );
+    assert_eq!( Some( vec![2, 3, 1] ), engine.next() );
+    assert_eq!( Some( vec![2, 1, 3] ), engine.next() );
+    assert_eq!( None, engine.next() );
+}
+
+
+#[test]
+fn try_with_loop() {
+    // input
+    let input = vec![1, 2, 3];
+    // call
+    let mut engine = PermutationEngine::new( input );
+
+    engine.for_each( move |permutation| println!( " permutation {:?}", permutation ));
 }
